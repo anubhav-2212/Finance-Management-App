e72af732afe88655eadcc2e488c7389f9eca36e1
Anubhav Srivastva <srivastvaanubhav.2212@gmail.com>
2025-09-18 23:39:06 +0530

    got Clerk UserID

--- Server/controllers/income.controller.js
+++ Server/controllers/income.controller.js
@@ -1,4 +1,11 @@
-export const  addIncome=async()=>{}
+export const  addIncome=async()=>{
+    const{userId}=req.auth
+    console.log(userId)
+    res.status(200).json({
+        success:true,
+        message:"Income added successfully"
+    })
+}
 export  const getAllIncome=async()=>{}
 export const deleteIncome=async()=>{}
 export const downloadIncomeExcel=async()=>{}
--- Server/routes/income.routes.js
+++ Server/routes/income.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { requireAuth } from "../middlewares/auth.middlewares";
-import { addIncome, deleteIncome, downloadIncomeExcel, getAllIncome } from "../controllers/income.controller";
+import { requireAuth } from "../middlewares/auth.middlewares.js";
+import { addIncome, deleteIncome, downloadIncomeExcel, getAllIncome } from "../controllers/income.controller.js";
 const incomeRoutes=express.Router();
 
 incomeRoutes.get('/add',requireAuth,addIncome)
 incomeRoutes.get('/get',requireAuth,getAllIncome)
 incomeRoutes.get('/delete/:id',requireAuth,deleteIncome)
 incomeRoutes.get('/downloadexcel',requireAuth,downloadIncomeExcel)
+
+export default incomeRoutes
